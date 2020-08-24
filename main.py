1b9ad2ad64e3918864ede71571a0c45cc4ead8fd
Filipe Toyoshima <filipetoyoshima@gmail.com>
2020-08-24 16:38:27 -0300

    loading assets

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import pyxel
 class Sprite(ABC):
 	_x = NotImplemented
 	_y = NotImplemented
+	_img = NotImplemented
+	_u = NotImplemented
+	_v = NotImplemented
+	_w = NotImplemented
+	_h = NotImplemented
 
 	@property
 	def x(self):
@@ -17,7 +22,15 @@ class Sprite(ABC):
 		pass
 
 	def draw(self):
-		pass
+		pyxel.blt(
+			x = self.x,
+			y = self.y,
+			img = self._img,
+			u = self._u,
+			v = self._v,
+			w = self._w,
+			h = self._h
+		)
 
 
 class Racket(Sprite):
@@ -26,6 +39,11 @@ class Racket(Sprite):
 			self._y = pyxel.height - 10
 			self._x = pyxel.width / 2
 			self._player = 1
+			self._u = 0
+			self._v = 0
+			self._img = 0
+			self._w = 16
+			self._h = 2
 
 	def update(self):
 		"""
@@ -36,14 +54,11 @@ class Racket(Sprite):
 		elif (pyxel.btn(pyxel.KEY_RIGHT) or pyxel.btn(pyxel.KEY_D)):
 			self._x = self.x + 1
 
-	def draw(self):
-		pyxel.pset(self.x, self.y, 11)
-			
-
 
 class Game(object):
 	def __init__(self):
-		pyxel.init (80, 120, caption="Super Pong")
+		pyxel.init(80, 120, caption="Super Pong")
+		pyxel.load('./resorces.pyxres')
 		self.racket = Racket(1)
 		pyxel.run(self.update, self.draw)
 
