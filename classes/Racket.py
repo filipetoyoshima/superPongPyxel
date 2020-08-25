d77c405df2e4934aa6bc51504d0fdb7008a27cd1
Filipe Toyoshima <filipetoyoshima@gmail.com>
2020-08-25 00:19:45 -0300

    changed to speed logig at Racket

--- classes/Racket.py
+++ classes/Racket.py
@@ -3,40 +3,40 @@ import pyxel
 
 class Racket(Sprite):
 	def __init__(self, player):
+		
+		self._speed = 0
+		initial_position = 0
+
 		if (player == 0):
-			super().__init__(
-				y = pyxel.height - 10,
-				x = pyxel.width / 2,
-				u = 0,
-				v = 0,
-				img = 0,
-				w = 16,
-				h = 2
-			)
+			initial_position = pyxel.height - 10
 			self._player = 1
 			self._left_key = pyxel.KEY_A
 			self._right_key = pyxel.KEY_D
 
 		elif (player == 1):
-			super().__init__(
-				y = 10,
-				x = pyxel.width / 2,
-				u = 0,
-				v = 0,
-				img = 0,
-				w = 16,
-				h = 2
-			)
+			initial_position = 10
 			self._player = 2
 			self._left_key = pyxel.KEY_LEFT
 			self._right_key = pyxel.KEY_RIGHT
-
+		
+		super().__init__(
+			y = initial_position,
+			x = pyxel.width / 2,
+			u = 0,
+			v = 0,
+			img = 0,
+			w = 16,
+			h = 2
+		)
 
 	def update(self):
 		"""
 		Updates the Racket attributes depending on user input
 		"""
+		speed = 0
 		if (pyxel.btn(self._left_key) and self.x > 8):
-			self._x = self.x - 1
-		elif (pyxel.btn(self._right_key) and self.x < pyxel.width - 8):
-			self._x = self.x + 1
+			speed -= 1
+		if (pyxel.btn(self._right_key) and self.x < pyxel.width - 8):
+			speed += 1
+
+		self._x = self.x + speed
